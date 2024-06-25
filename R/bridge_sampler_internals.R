02a7b47cdd5339c2b56c7c968bee77c5b9631899
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-06-25 15:06:10 +0300

    Update bridge_sampler_internals.R

--- R/bridge_sampler_internals.R
+++ R/bridge_sampler_internals.R
@@ -372,10 +372,11 @@
     print("Log Likelihood estimation failed")
     if (return_always == TRUE){
       pareto_k <- .pareto_k_diagnostic(numi, deni)
+      return(list(logml = NA, niter = i-1, numi = numi, deni = deni, pareto_k = pareto_k))
     } else {
       pareto_k <- list(numi = NA, deni = NA)
+      return(list(logml = logml, niter = i-1, numi = numi, deni = deni, pareto_k = pareto_k))
     }
-    return(list(logml = NA, niter = i-1, numi = numi, deni = deni, pareto_k = pareto_k))
   }
   
   pareto_k <- .pareto_k_diagnostic(numi, deni)
