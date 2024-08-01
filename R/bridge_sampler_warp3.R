18fd61e195e3f5376251a52c6309aec1945b748e
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-08-01 12:39:43 +0300

    Update bridge_sampler_warp3.R

--- R/bridge_sampler_warp3.R
+++ R/bridge_sampler_warp3.R
@@ -23,7 +23,8 @@
   silent,
   verbose,
   r0,
-  pareto_smoothing,
+  pareto_smoothing_all,
+  pareto_smoothing_last,
   tol1,
   tol2,
   return_always) {
@@ -195,17 +196,17 @@
   # run iterative updating scheme to compute log of marginal likelihood
   for (i in seq_len(repetitions)) {
     tmp <- .run.iterative.scheme(q11 = q11, q12 = q12, q21 = q21[[i]], q22 = q22[[i]],
-                                 r0 = r0, tol = tol1, L = L, method = "warp3",
-                                 maxiter = maxiter, silent = silent, pareto_smoothing = pareto_smoothing,
+                                 r0 = r0, tol = tol1, L = L, method = "warp3", pareto_smoothing_all = pareto_smoothing_all,
+                                 maxiter = maxiter, silent = silent, pareto_smoothing_last = pareto_smoothing_all,
                                  criterion = "r", neff = neff, return_always=return_always)
     if (is.na(tmp$logml) & !is.null(tmp$r_vals)) {
       warning("logml could not be estimated within maxiter, rerunning with adjusted starting value. \nEstimate might be more variable than usual.", call. = FALSE)
       lr <- length(tmp$r_vals)
       # use geometric mean as starting value
       r0_2 <- sqrt(tmp$r_vals[[lr - 1]] * tmp$r_vals[[lr]])
-      tmp <- .run.iterative.scheme(q11 = q11, q12 = q12, q21 = q21[[i]], q22 = q22[[i]],
-                                   r0 = r0_2, tol = tol2, L = L, method = "warp3",
-                                   maxiter = maxiter, silent = silent, pareto_smoothing = pareto_smoothing,
+      tmp <- .run.iterative.scheme(q11 = q11, q12 = q12, q21 = q21[[i]], q22 = q22[[i]], 
+                                   r0 = r0_2, tol = tol2, L = L, method = "warp3", pareto_smoothing_last = pareto_smoothing_last,
+                                   maxiter = maxiter, silent = silent, pareto_smoothing_all = pareto_smoothing_all,
                                    criterion = "logml", neff = neff, return_always=return_always)
       tmp$niter <- maxiter + tmp$niter
     }
