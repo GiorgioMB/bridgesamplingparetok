258e2d879967245638e5bc8129b2d112cebc18d7
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-08-08 14:18:28 +0300

    Update bridge_sampler_tools.R

--- R/bridge_sampler_tools.R
+++ R/bridge_sampler_tools.R
@@ -110,6 +110,7 @@
   net
 }
 
+# Define the RealNVP model
 .create_realnvp <- function(input_shape, num_coupling_layers) {
   net <- nn_module(
     initialize = function() {
@@ -131,7 +132,7 @@
   net
 }
 
-
+# Define the negative log likelihood function
 .negative_log_likelihood <- function(y_true, y_pred) {
   z <- y_pred[[1]]
   log_det_jacobian <- y_pred[[2]]
@@ -141,21 +142,25 @@
   nll
 }
 
-
-.train_realnvp <- function(samples, normal_samples, num_coupling_layers = 5, epochs = 50, batch_size = 32, learning_rate = 0.001, train_ratio = 0.8) {
-  library(torch)                                                                                                                                            
+# Define the training function for RealNVP
+.train_realnvp <- function(samples, normal_samples, num_coupling_layers = 5, epochs = 50, batch_size = 32, learning_rate = 0.001, train_ratio = 0.8, verbose = FALSE) {
+  library(torch)
   input_shape <- ncol(samples)
   realnvp_model <- .create_realnvp(input_shape, num_coupling_layers)
-  
+  if(verbose){
+    print(realnvp_model)
+  }
   optimizer <- optim_adam(realnvp_model$parameters, lr = learning_rate)
-  
   for (epoch in seq_len(epochs)) {
     realnvp_model$zero_grad()
     outputs <- realnvp_model(samples)
     loss <- .negative_log_likelihood(normal_samples, outputs)
     loss$backward()
     optimizer$step()
+    # Print the loss every 10 iterations, to have an idea of how the model is going
+    if (epoch %% 10 == 0 && verbose) {
+      cat(sprintf("Epoch [%d/%d], Loss: %f\n", epoch, epochs, loss$item()))
+    }
   }
-  
   realnvp_model
-}                                  
+}
