6a3dfc5ceae941ec3f2ed28b4cbda93bef44a841
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-08-08 13:35:35 +0300

    Update bridge_sampler_tools.R

--- R/bridge_sampler_tools.R
+++ R/bridge_sampler_tools.R
@@ -76,77 +76,102 @@
 #--------------------------------------------------------------------------
 # functions for RealNVP support
 #--------------------------------------------------------------------------
-.create_affine_coupling_layer <- function(input_shape) {
-  input <- tensorflow::tf$keras$layers$Input(shape = input_shape)
+create_affine_coupling_layer <- function(input_shape) {
+  # Define the input layer
+  input <- nnf_input(input_shape)
+  
+  # Split the input tensor
   d <- as.integer(input_shape / 2)
-  x1 <- tensorflow::tf$keras$layers$Lambda(function(x) x[, 1:d])(input)
-  x2 <- tensorflow::tf$keras$layers$Lambda(function(x) x[, (d + 1):input_shape])(input)
+  x1 <- nn_lambda(function(x) x[, 1:d], output_shape = c(d))
+  x2 <- nn_lambda(function(x) x[, (d + 1):input_shape], output_shape = c(d))
   
-  scale_translation_network <- tensorflow::tf$keras$models$Sequential() %>%
-    tensorflow::tf$keras$layers$Dense(units = 512, activation = 'relu') %>%
-    tensorflow::tf$keras$layers$Dense(units = d * 2)
-
+  # Scale and translation network
+  scale_translation_network <- nn_sequential(
+    nn_linear(d, 512),
+    nn_relu(),
+    nn_linear(512, d * 2)
+  )
+  
   st <- scale_translation_network(x1)
-  s <- tensorflow::tf$keras$layers$Lambda(function(x) x[, 1:d])(st)
-  t <- tensorflow::tf$keras$layers$Lambda(function(x) x[, (d + 1):(2 * d)])(st)
-
+  s <- nn_lambda(function(x) x[, 1:d], output_shape = c(d))(st)
+  t <- nn_lambda(function(x) x[, (d + 1):(2 * d)], output_shape = c(d))(st)
+  
   y1 <- x1
-  y2 <- x2 * tensorflow::tf$exp(s) + t
+  y2 <- x2 * exp(s) + t
   
-  log_det_jacobian <- tensorflow::tf$reduce_sum(s, axis = 1)  # sum log(s) for Jacobian determinant
+  # Compute the log determinant of the Jacobian
+  log_det_jacobian <- torch_sum(s, dim = 2)
   
-  output <- tensorflow::tf$keras$layers$Concatenate()(list(y1, y2))
-  return(tensorflow::tf$keras$Model(inputs = input, outputs = list(output, log_det_jacobian)))
+  # Concatenate y1 and y2
+  output <- nn_concatenate(dim = 2)(list(y1, y2))
+  
+  model <- nn_module(
+    initialize = function() {
+      self$input <- input
+      self$output <- output
+    },
+    forward = function(x) {
+      list(self$output(x), log_det_jacobian)
+    }
+  )
+  
+  return(model)
 }
-                                          
-.create_realnvp <- function(input_shape, num_coupling_layers) {
-  inputs <- tensorflow::tf$keras$layers$Input(shape = input_shape)
+
+create_realnvp <- function(input_shape, num_coupling_layers) {
+  inputs <- nnf_input(input_shape)
   x <- inputs
   total_log_det_jacobian <- 0
-
+  
   for (i in seq_len(num_coupling_layers)) {
-    coupling_layer <- .create_affine_coupling_layer(input_shape)
+    coupling_layer <- create_affine_coupling_layer(input_shape)
     result <- coupling_layer(x)
     x <- result[[1]]  # updated x
     total_log_det_jacobian <- total_log_det_jacobian + result[[2]]  # accumulate the log determinant of Jacobian
   }
-
-  model <- tensorflow::tf$keras$Model(inputs = inputs, outputs = list(x, total_log_det_jacobian))
+  
+  model <- nn_module(
+    initialize = function() {
+      self$inputs <- inputs
+      self$x <- x
+      self$total_log_det_jacobian <- total_log_det_jacobian
+    },
+    forward = function(x) {
+      list(self$x(x), self$total_log_det_jacobian)
+    }
+  )
+  
   return(model)
 }
 
-.negative_log_likelihood <- function(y_true, y_pred) {
+negative_log_likelihood <- function(y_true, y_pred) {
   z <- y_pred[[1]]
   log_det_jacobian <- y_pred[[2]]
-  logp_z <- -0.5 * tensorflow::tf$reduce_sum(z^2, axis = 1L) - 0.5 * length(z) * log(2 * pi)
-  nll <- -tensorflow::tf$reduce_mean(logp_z + log_det_jacobian)
+  logp_z <- -0.5 * torch_sum(z^2, dim = 1L) - 0.5 * length(z) * log(2 * pi)
+  nll <- -mean(logp_z + log_det_jacobian)
   return(nll)
 }
 
-                                          
-
-                                          
-.train_realnvp <- function(samples, normal_samples, num_coupling_layers = 5, epochs = 50, batch_size = 32, learning_rate = 0.001, train_ratio = 0.8) {
+train_realnvp <- function(samples, normal_samples, num_coupling_layers = 5, epochs = 50, batch_size = 32, learning_rate = 0.001, train_ratio = 0.8) {
+  library(torch)
   input_shape <- ncol(samples)
+  realnvp_model <- create_realnvp(input_shape, num_coupling_layers)
+  optimizer <- optim_adam(realnvp_model$parameters, lr = learning_rate)
   
-  realnvp_model <- .create_realnvp(input_shape, num_coupling_layers)
+  dataset <- tensor_dataset(list(samples, normal_samples))
+  dataloader <- dataloader(dataset, batch_size = batch_size, shuffle = TRUE)
   
-  realnvp_model %>% keras::compile(
-    optimizer = keras::optimizer_adam(lr = learning_rate),
-    loss = .negative_log_likelihood
-  )
-
-  history <- realnvp_model %>% keras::fit(
-    x = train_samples,
-    y = normal_samples,  
-    epochs = epochs,
-    batch_size = batch_size,
-    shuffle = TRUE,
-    validation_data = list(test_samples, test_samples)
-  )
+  # Training loop
+  for (epoch in seq_len(epochs)) {
+    for (batch in enumerate(dataloader)) {
+      optimizer$zero_grad()
+      output <- realnvp_model(batch[[1]])
+      loss <- negative_log_likelihood(batch[[2]], output)
+      loss$backward()
+      optimizer$step()
+    }
+  }
   
   return(realnvp_model)
-}
-
-                                            
+}                                      
                                          
