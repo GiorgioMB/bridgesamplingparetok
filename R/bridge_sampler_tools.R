9827446879b9414840b0b775cb26a113e9664350
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-08-08 16:44:12 +0300

    Update bridge_sampler_tools.R

--- R/bridge_sampler_tools.R
+++ R/bridge_sampler_tools.R
@@ -136,9 +136,6 @@
   log_det_jacobian <- y_pred[[2]]
   logp_z <- -0.5 * torch_sum(z^2, dim = 2L) - 0.5 * ncol(z) * log(2 * pi)
   nll <- -torch_mean(logp_z + log_det_jacobian)
-  mse <- torch_mean((y_true - z)^2)
-  
-  nll + mse
 }
 
 .train_realnvp <- function(samples, normal_samples, num_coupling_layers = 5, epochs = 50, learning_rate = 0.001, verbose = FALSE) {
