c0ccbd4beef70a1447ef7f9fc2843cadb112b325
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-09-12 15:30:59 +0200

    Update bridge_sampler_tools.R

--- R/bridge_sampler_tools.R
+++ R/bridge_sampler_tools.R
@@ -77,112 +77,118 @@
   return(out)
 }
 
+
+                  
 #--------------------------------------------------------------------------
 # functions for RealNVP support / DEPRECATED
 #--------------------------------------------------------------------------
-#.create_affine_coupling_layer <- function(input_shape) {
-#  d <- as.integer(input_shape / 2)
-#  scale_translation_network <- nn_sequential(
-#    nn_linear(d, 512),
-#    nn_relu(),
-#    nn_linear(512, d * 2)
-#  )
-#  net <- nn_module(
-#    initialize = function() {
-#      self$scale_translation_network <- scale_translation_network
-#    },
-#    forward = function(x) {
-#      x1 <- x[, 1:d]
-#      x2 <- x[, (d + 1):input_shape]
-#      
-#      st <- self$scale_translation_network(x1)
-#      s <- st[, 1:d]
-#      t <- st[, (d + 1):(2 * d)]
-#      
-#      y1 <- x1
-#      y2 <- x2 * torch_exp(s) + t
-#      
-#      log_det_jacobian <- torch_sum(s, dim = 2) 
-#      
-#      output <- torch_cat(list(y1, y2), dim = 2)
-#      list(output, log_det_jacobian)
-#    }
-#  )
-#  
-#  net()
-#}
 
-#.create_realnvp <- function(input_shape, num_coupling_layers) {
-#  net <- nn_module(
-#    initialize = function() {
-#      self$coupling_layers <- nn_module_list()
-#      for (i in seq_len(num_coupling_layers)) {
-#        self$coupling_layers$append(.create_affine_coupling_layer(input_shape))
-#      }
-#    },
-#    forward = function(x) {
-#      total_log_det_jacobian <- 0
-#      
-#      for (i in seq_len(length(self$coupling_layers))) {
-#        result <- self$coupling_layers[[i]](x)
-#        x <- result[[1]]  # updated x
-#        total_log_det_jacobian <- total_log_det_jacobian + result[[2]]
-#      }
-#      
-#      list(x, total_log_det_jacobian)
-#    }
-#  )
+.create_affine_coupling_layer <- function(input_shape) {
+  d <- as.integer(input_shape / 2)
+  scale_translation_network <- nn_sequential(
+    nn_linear(d, 512),
+    nn_relu(),
+    nn_linear(512, d * 2)
+  )
+  net <- nn_module(
+    initialize = function() {
+      self$scale_translation_network <- scale_translation_network
+    },
+    forward = function(x) {
+      x1 <- x[, 1:d]
+      x2 <- x[, (d + 1):input_shape]
+      
+      st <- self$scale_translation_network(x1)
+      s <- st[, 1:d]
+      t <- st[, (d + 1):(2 * d)]
+      
+      y1 <- x1
+      y2 <- x2 * torch_exp(s) + t
+      
+      log_det_jacobian <- torch_sum(s, dim = 2) 
+      
+      output <- torch_cat(list(y1, y2), dim = 2)
+      list(output, log_det_jacobian)
+    }
+  )
   
-#  net()
-#}
+  net()
+}
 
 
-#.negative_log_likelihood <- function(y_true, y_pred) {
-#  z <- y_pred[[1]]
-#  log_det_jacobian <- y_pred[[2]]
-#  logp_z <- -0.5 * torch_sum(z^2, dim = 2L) - 0.5 * ncol(z) * log(2 * pi)
-#  nll <- -torch_mean(logp_z + log_det_jacobian)
-#}
 
-#.train_realnvp <- function(samples, normal_samples, num_coupling_layers = 5, epochs = 50, learning_rate = 0.001, verbose = FALSE) {
-#  library(torch)
-#  input_shape <- ncol(samples)
-#  realnvp_model <- .create_realnvp(input_shape, num_coupling_layers)
-#  optimizer <- optim_adam(realnvp_model$parameters, lr = learning_rate)
-#  
-#  for (epoch in seq_len(epochs)) {
-#    realnvp_model$zero_grad()
-#    outputs <- realnvp_model(samples)
-#    loss <- .negative_log_likelihood(normal_samples, outputs)
-#    loss$backward()
-#    optimizer$step()
-#    
-#    if(verbose){
-#      if (epoch %% 10 == 0) {
-#        cat(sprintf("Epoch [%d/%d], Loss: %f\n", epoch, epochs, loss$item()))
-#      }
-#    }
-#  }
-#  
-#  realnvp_model
-#}
-                  
-#.transform_to_normal <- function(samples) {
-#  n <- nrow(samples)
-#  m <- ncol(samples)
-#  
-#  # Initialize the output matrix
-#  transformed <- matrix(nrow = n, ncol = m)
-#  
-#  for (j in 1:m) {
-#    column_data <- samples[, j]
-#    sorted_data <- sort(column_data)
-#    ranks <- rank(column_data, ties.method = "average")  
-#    
-#    ecdf_values <- (ranks - 0.5) / n  
-#    transformed[, j] <- qnorm(ecdf_values)
-#  }
-#  
-#  return(transformed)
-#}
-#
+.create_realnvp <- function(input_shape, num_coupling_layers) {
+  net <- nn_module(
+    initialize = function() {
+      self$coupling_layers <- nn_module_list()
+      for (i in seq_len(num_coupling_layers)) {
+        self$coupling_layers$append(.create_affine_coupling_layer(input_shape))
+      }
+    },
+    forward = function(x) {
+      total_log_det_jacobian <- 0
+      
+      for (i in seq_len(length(self$coupling_layers))) {
+        result <- self$coupling_layers[[i]](x)
+        x <- result[[1]]  # updated x
+        total_log_det_jacobian <- total_log_det_jacobian + result[[2]]
+      }
+      
+      list(x, total_log_det_jacobian)
+    }
+  )
+ 
+  net()
+}
+
+
+.negative_log_likelihood <- function(y_true, y_pred) {
+  z <- y_pred[[1]]
+  log_det_jacobian <- y_pred[[2]]
+  logp_z <- -0.5 * torch_sum(z^2, dim = 2L) - 0.5 * ncol(z) * log(2 * pi)
+  nll <- -torch_mean(logp_z + log_det_jacobian)
+}
+
+.train_realnvp <- function(samples, normal_samples, num_coupling_layers = 5, epochs = 50, learning_rate = 0.001, verbose = FALSE) {
+  library(torch)
+  input_shape <- ncol(samples)
+  realnvp_model <- .create_realnvp(input_shape, num_coupling_layers)
+  optimizer <- optim_adam(realnvp_model$parameters, lr = learning_rate)
+  
+  for (epoch in seq_len(epochs)) {
+    realnvp_model$zero_grad()
+    outputs <- realnvp_model(samples)
+    loss <- .negative_log_likelihood(normal_samples, outputs)
+    loss$backward()
+    optimizer$step()
+    if(verbose){
+      if (epoch %% 10 == 0) {
+        cat(sprintf("Epoch [%d/%d], Loss: %f\n", epoch, epochs, loss$item()))
+      }
+    }
+  }
+  
+  realnvp_model
+}
+                 
+.transform_to_normal <- function(samples, num_coupling_layers = 5, epochs = 50, learning_rate = 0.001, verbose = FALSE, seed = 1) {
+    set.seed(seed)
+    n <- nrow(samples)
+    d <- ncol(samples)
+    if(mean(samples) != 0 || sd(samples) != 1) {
+        samples <- (samples - mean(samples)) / sd(samples)
+    }
+    samples <- rep(samples, 2)
+    samples <- matrix(samples, ncol = d * 2)
+    if(!inherits(samples, "torch_tensor")) {
+        samples <- torch::torch_tensor(samples)
+    }
+    normal_samples <- rnorm(n * d)
+    normal_samples <- matrix(normal_samples, nrow = n, ncol = d)
+    normal_samples <- torch::torch_tensor(normal_samples)
+    trained_rnvp <- .train_realnvp(samples, normal_samples, num_coupling_layers, epochs, learning_rate, verbose)
+    x_rnvp_warped <- as.matrix(trained_rnvp$forward(samples)[[1]])
+    x_log_det_jacobian <- as.matrix(trained_rnvp$forward(samples)[[2]])
+
+    list(x_rnvp_warped, x_log_det_jacobian)
+}
