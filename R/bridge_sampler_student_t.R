3f50e094db5d71fc1c8793605c91d156de1b88cb
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-09-15 21:40:39 +0200

    Update bridge_sampler_student_t.R

--- R/bridge_sampler_student_t.R
+++ R/bridge_sampler_student_t.R
@@ -37,7 +37,7 @@
   V_tmp <- cov(samples_4_fit)
   V <- as.matrix(nearPD(V_tmp)$mat) # Ensure V is positive-definite
 
-  df <- .estimate_df(samples_4_fit, m, V)
+  df <- .estimate_df(samples_4_fit)
   print(df)
   # Sampling and density computation using Student t-distribution
   q12 <- mvtnorm::dmvt(samples_4_iter, delta = m, sigma = V, df = df, log = TRUE)
