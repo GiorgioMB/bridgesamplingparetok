a7f04ccbf2d5410e0e98b2238b80f0f6bde2a994
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-09-24 13:12:44 +0200

    Update bridge_sampler_warp3.R

--- R/bridge_sampler_warp3.R
+++ R/bridge_sampler_warp3.R
@@ -197,7 +197,6 @@
   std_logmls <- numeric(repetitions)
   pareto_k_numi <- list()
   pareto_k_deni <- list()
-  pareto_k_l1 <- list()
   pareto_k_inv_deni <- list()
   numi <- list()
   deni <- list()
@@ -230,7 +229,6 @@
       }
       pareto_k_numi[[i]] <- tmp$pareto_k$numi
       pareto_k_deni[[i]] <- tmp$pareto_k$deni
-      pareto_k_l1 <- tmp$pareto_k$l1
       pareto_k_inv_deni[[i]] <- tmp$pareto_k$inv_deni
     } else {
       if(verbose){
@@ -238,7 +236,6 @@
       }
       pareto_k_numi[[i]] <- NA
       pareto_k_deni[[i]] <- NA
-      pareto_k_l1[[i]] <- NA
       pareto_k_inv_deni[[i]] <- NA
     }
     
@@ -249,12 +246,12 @@
   if (repetitions == 1) {
     out <- list(logml = logml, niter = niter, method = "warp3", q11 = q11,
                 q12 = q12, q21 = q21[[1]], q22 = q22[[1]], pareto_k_numi = pareto_k_numi,
-                pareto_k_deni = pareto_k_deni, pareto_k_l1 = pareto_k_l1, pareto_k_inv_deni = pareto_k_inv_deni, 
+                pareto_k_deni = pareto_k_deni, pareto_k_inv_deni = pareto_k_inv_deni, 
                 mcse_logml = std_logmls)
     class(out) <- "bridge"
   } else if (repetitions > 1) {
     out <- list(logml = logml, niter = niter, method = "warp3", repetitions = repetitions,
-                pareto_k_numi = pareto_k_numi, pareto_k_deni = pareto_k_deni, pareto_k_l1 = pareto_k_l1,
+                pareto_k_numi = pareto_k_numi, pareto_k_deni = pareto_k_deni,
                 pareto_k_inv_deni = pareto_k_inv_deni, mcse_logml = std_logmls)
     class(out) <- "bridge_list"
   }
