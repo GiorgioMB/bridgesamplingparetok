96c69db4f9064500f7bd13ae13d18da729bd14d0
Aki Vehtari <Aki.Vehtari@aalto.fi>
2026-09-16 09:53:10 +0300

    Restore upstream R sources and remove Ledoit-Wolf optimal-alpha machinery

--- R/bridge_sampler_tools.R
+++ R/bridge_sampler_tools.R
@@ -1,77 +1,64 @@
 #--------------------------------------------------------------------------
-# functions for Stan support
+# functions for Stan support via rstan
 #--------------------------------------------------------------------------
 
 # taken from rstan:
-.rstan_relist <- function (x, skeleton) {
+.rstan_relist <- function(x, skeleton) {
   lst <- utils::relist(x, skeleton)
-  for (i in seq_along(skeleton)) dim(lst[[i]]) <- dim(skeleton[[i]])
+  for (i in seq_along(skeleton)) {
+    dim(lst[[i]]) <- dim(skeleton[[i]])
+  }
   lst
 }
 
 # taken from rstan:
-.create_skeleton <- function (pars, dims) {
+.create_skeleton <- function(pars, dims) {
   lst <- lapply(seq_along(pars), function(i) {
     len_dims <- length(dims[[i]])
-    if (len_dims < 1)
+    if (len_dims < 1) {
       return(0)
+    }
     return(array(0, dim = dims[[i]]))
   })
   names(lst) <- pars
   lst
 }
 
-.stan_log_posterior <- function(s.row, data, keep_log_eval) {
-  out <- tryCatch(rstan::log_prob(object = data$stanfit, upars = s.row), error = function(e) -Inf)
-  if (is.na(out)) out <- -Inf
-  result <- data.frame(matrix(s.row, nrow = 1))
-  result$log_posterior <- out
-  csv_file <- "rstan_log_eval.csv"
-  # Append the result to the CSV file
-  if (keep_log_eval) {
-    if (!file.exists(csv_file)) {
-      write.csv(result, file = csv_file, row.names = FALSE)
-    } else {
-      write.table(result, file = csv_file, row.names = FALSE, col.names = FALSE, append = TRUE, sep = ",")
-    }
+.stan_log_posterior <- function(s.row, data) {
+  out <- tryCatch(
+    rstan::log_prob(object = data$stanfit, upars = s.row),
+    error = function(e) -Inf
+  )
+  if (is.na(out)) {
+    out <- -Inf
   }
   return(out)
 }
-                  
-.cmdstan_log_posterior <- function(s.row, data, keep_log_eval) {
-  if("lp__" %in% names(s.row)) {
+
+.cmdstan_log_posterior <- function(s.row, data) {
+  if ("lp__" %in% names(s.row)) {
     s.row <- s.row[!names(s.row) %in% "lp__"]
-    }
-  #add a print that checks if fit is a CmdStanMCMC object
-  if(!inherits(data, "CmdStanMCMC")) {
-        stop("Passed model must be a CmdStanMCMC object")
   }
-  if(!is.numeric(s.row)) {
+
+  if (!is.numeric(s.row)) {
     s.row <- as.numeric(s.row)
   }
-  out <- tryCatch({
-    log_prob <- data$log_prob(s.row)
-    log_prob
-  }, error = function(e) {
-    print(e)
-    -Inf
-  })
-  
+  out <- tryCatch(
+    {
+      log_prob <- data$log_prob(s.row, jacobian = TRUE)
+      log_prob
+    },
+    error = function(e) {
+      print(e)
+      -Inf
+    }
+  )
 
   if (is.na(out)) {
     out <- -Inf
   }
   result <- data.frame(matrix(s.row, nrow = 1))
   result$log_posterior <- out
-  csv_file <- "cmdstanr_log_eval.csv"
-  # Append the result to the CSV file
-  if (keep_log_eval) {
-    if (!file.exists(csv_file)) {
-      write.csv(result, file = csv_file, row.names = FALSE)
-    } else {
-      write.table(result, file = csv_file, row.names = FALSE, col.names = FALSE, append = TRUE, sep = ",")
-    }
-  }
-  
+
   return(out)
 }
