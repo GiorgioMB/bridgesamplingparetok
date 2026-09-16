5efc3a2bba7955ab4e3da9ea388ef8c9e9471c6a
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2026-09-15 20:19:56 -0500

    Remove pareto_k diagnostics from bridge_sampler

--- R/bridge_sampler_normal.R
+++ R/bridge_sampler_normal.R
@@ -18,8 +18,6 @@
   varlist,
   envir,
   rcppFile,
-  pareto_smoothing_all,
-  pareto_smoothing_last,
   maxiter,
   silent,
   verbose,
@@ -314,16 +312,13 @@
   logml <- numeric(repetitions)
   niter <- numeric(repetitions)
   std_logmls <- numeric(repetitions)
-  pareto_k_numi <- list()
-  pareto_k_deni <- list()
-  pareto_k_inv_deni <- list()
   numi <- list()
   deni <- list()
   # run iterative updating scheme to compute log of marginal likelihood
   for (i in seq_len(repetitions)) {
     tmp <- .run.iterative.scheme(q11 = q11, q12 = q12, q21 = q21[[i]], q22 = q22[[i]],
-                                 r0 = r0, tol = tol1, L = NULL, method = "normal",pareto_smoothing_all = pareto_smoothing_all, use_ess = use_ess,
-                                 maxiter = maxiter, silent = silent, pareto_smoothing_last = pareto_smoothing_last, verbose = verbose,
+                                 r0 = r0, tol = tol1, L = NULL, method = "normal", use_ess = use_ess,
+                                 maxiter = maxiter, silent = silent, verbose = verbose,
                                  criterion = "r", neff = neff, return_always = return_always, calculate_covariance = calculate_covariance)
     if (!is.null(tmp$r_vals)) {
       warning("logml could not be estimated within maxiter, rerunning with adjusted starting value. \nEstimate might be more variable than usual.", call. = FALSE)
@@ -331,8 +326,8 @@
       # use geometric mean as starting value
       r0_2 <- sqrt(tmp$r_vals[[lr - 1]] * tmp$r_vals[[lr]])
       tmp <- .run.iterative.scheme(q11 = q11, q12 = q12, q21 = q21[[i]], q22 = q22[[i]], use_ess = use_ess,
-                                   r0 = r0_2, tol = tol2, L = NULL, method = "normal", pareto_smoothing_all = pareto_smoothing_all, verbose = verbose,
-                                   maxiter = maxiter, silent = silent, return_always = return_always, pareto_smoothing_last = pareto_smoothing_last,
+                                   r0 = r0_2, tol = tol2, L = NULL, method = "normal", verbose = verbose,
+                                   maxiter = maxiter, silent = silent, return_always = return_always,
                                    criterion = "logml", neff = neff, calculate_covariance = calculate_covariance)
       tmp$niter <- maxiter + tmp$niter
     }
@@ -341,21 +336,6 @@
     std_logmls[i] <- tmp$std_logml
     numi[[i]] <- tmp$numi
     deni[[i]] <- tmp$deni
-    if("pareto_k" %in% names(tmp)) {
-      if(verbose){
-        print(tmp$pareto_k)
-      }
-      pareto_k_numi[[i]] <- tmp$pareto_k$numi
-      pareto_k_deni[[i]] <- tmp$pareto_k$deni
-      pareto_k_inv_deni[[i]] <- tmp$pareto_k$inv_deni
-    } else {
-      if(verbose){
-        print("There was an error computing the pareto_k diagnostic")
-      }
-      pareto_k_numi[[i]] <- NA
-      pareto_k_deni[[i]] <- NA
-      pareto_k_inv_deni[[i]] <- NA
-    }
       
     if (niter[i] == maxiter)
       warning("logml could not be estimated within maxiter.", call. = FALSE)
@@ -363,13 +343,11 @@
 
   if (repetitions == 1) {
     out <- list(logml = logml, niter = niter, method = "normal", q11 = q11, numi = numi, deni = deni,
-              q12 = q12, q21 = q21[[1]], q22 = q22[[1]], pareto_k_numi = pareto_k_numi, 
-              pareto_k_deni = pareto_k_deni, pareto_k_inv_deni = pareto_k_inv_deni, mcse_logml = std_logmls,
+              q12 = q12, q21 = q21[[1]], q22 = q22[[1]], mcse_logml = std_logmls,
               proposal_fit_info = proposal_fit_info)
     class(out) <- "bridge"
   } else if (repetitions > 1) {
     out <- list(logml = logml, niter = niter, method = "normal", repetitions = repetitions, numi = numi, deni = deni,
-              pareto_k_numi = pareto_k_numi, pareto_k_deni = pareto_k_deni, pareto_k_inv_deni = pareto_k_inv_deni, 
               mcse_logml = std_logmls, proposal_fit_info = proposal_fit_info)
     class(out) <- "bridge_list"
   }
